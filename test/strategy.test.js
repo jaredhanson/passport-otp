94656f44466f08791b9d78afaf20bb7fc2d8c446
Jared Hanson <jaredhanson@gmail.com>
2017-11-03 16:21:19 -0700

    Add tests.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -1,19 +1,31 @@
 /* global describe, it, expect */
 
 var Strategy = require('../lib/strategy');
+var Algorithms = require('../lib/algorithms');
 
 
 describe('Strategy', function() {
-    
-  var strategy = new Strategy(function(){});
-    
+  
   it('should be named otp', function() {
+    var strategy = new Strategy(new Algorithms(), function(){});
     expect(strategy.name).to.equal('otp');
+  });
+  
+  it('should throw if constructed with no arguments', function() {
+    expect(function() {
+      var s = new Strategy();
+    }).to.throw(TypeError, 'OTPStrategy requires an algorithm registry');
+  });
+  
+  it('should throw if constructed without an algorithm registry', function() {
+    expect(function() {
+      var s = new Strategy(function(){});
+    }).to.throw(TypeError, 'OTPStrategy requires an algorithm registry');
   });
   
   it('should throw if constructed without a fetch callback', function() {
     expect(function() {
-      var s = new Strategy();
+      var s = new Strategy(new Algorithms());
     }).to.throw(TypeError, 'OTPStrategy requires a fetch callback');
   });
   
