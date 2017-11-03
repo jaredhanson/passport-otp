a10c60d3864289fd7c02931e4aca9bbffdd69cd4
Jared Hanson <jaredhanson@gmail.com>
2017-11-03 11:55:27 -0700

    Stub out tests.

--- test/package.test.js
+++ test/package.test.js
@@ -1,13 +1,14 @@
 /* global describe, it */
 
-var pkg = require('..');
+var strategy = require('..');
 var expect = require('chai').expect;
 
 
-describe('passport-mfa-otp', function() {
+describe('passport-otp', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  it('should export Strategy constructor directly from package', function() {
+    expect(strategy).to.be.a('function');
+    expect(strategy).to.equal(strategy.Strategy);
   });
   
 });
