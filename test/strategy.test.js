75780b5bfc8b549a7ceb934de2dcd9b9398285bb
Jared Hanson <jaredhanson@gmail.com>
2017-11-03 17:07:42 -0700

    More tests.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -247,4 +247,186 @@ describe('Strategy', function() {
     });
   }); // handling an request without otp parameter
   
+  describe('handling an request without body', function() {
+    var algorithms = new Algorithms();
+    var algo = {
+      verify: function(authnr, otp, cb) {
+      }
+    }
+    algorithms.use('example', algo);
+    
+    var strategy = new Strategy(algorithms, function(uid, done) {
+    });
+    
+    var info, status;
+    
+    before(function(done) {
+      chai.passport.use(strategy)
+        .fail(function(i, s) {
+          info = i;
+          status = s;
+          done();
+        })
+        .req(function(req) {
+        })
+        .authenticate();
+    });
+    
+    it('should fail with info and status', function() {
+      expect(info).to.be.an.object;
+      expect(info.message).to.equal('Missing credentials');
+      expect(status).to.equal(400);
+    });
+  }); // handling an request without body
+  
+  describe('encountering an error while fetching user and authenticator', function() {
+    var algorithms = new Algorithms();
+    var algo = {
+      verify: function(authnr, otp, cb) {
+        return cb(null, true);
+      }
+    }
+    algorithms.use('example', algo);
+    
+    var strategy = new Strategy(algorithms, function(uid, done) {
+      return done(new Error('something went wrong'));
+    });
+    
+    var err;
+    
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.body = { uid: '501', otp: '432028' };
+        })
+        .authenticate();
+    });
+    
+    it('should error', function() {
+      expect(err).to.be.an.instanceof(Error);
+      expect(err.message).to.equal('something went wrong');
+    });
+  }); // encountering an error while fetching user and authenticator
+  
+  describe('encountering an exception while fetching user and authenticator', function() {
+    var algorithms = new Algorithms();
+    var algo = {
+      verify: function(authnr, otp, cb) {
+        return cb(null, true);
+      }
+    }
+    algorithms.use('example', algo);
+    
+    var strategy = new Strategy(algorithms, function(uid, done) {
+      throw new Error('something went horribly wrong');
+    });
+    
+    var err;
+    
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.body = { uid: '501', otp: '432028' };
+        })
+        .authenticate();
+    });
+    
+    it('should error', function() {
+      expect(err).to.be.an.instanceof(Error);
+      expect(err.message).to.equal('something went horribly wrong');
+    });
+  }); // encountering an exception while fetching user and authenticator
+  
+  describe('encountering an error while verifying OTP', function() {
+    var algorithms = new Algorithms();
+    var algo = {
+      verify: function(authnr, otp, cb) {
+        return cb(new Error('failed to verify OTP'));
+      }
+    }
+    algorithms.use('example', algo);
+    
+    var strategy = new Strategy(algorithms, function(uid, done) {
+      var user = {
+        id: '501',
+        displayName: 'John Doe'
+      };
+      var authnr = {
+        id: 'dev_324598',
+        name: "Example Authenticator",
+        algorithm: 'example'
+      };
+      return done(null, user, authnr);
+    });
+    
+    var err;
+    
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.body = { uid: '501', otp: '432028' };
+        })
+        .authenticate();
+    });
+    
+    it('should error', function() {
+      expect(err).to.be.an.instanceof(Error);
+      expect(err.message).to.equal('failed to verify OTP');
+    });
+  }); // encountering an error while verifying OTP
+  
+  describe('failing due to unsupported algorithm', function() {
+    var algorithms = new Algorithms();
+    var algo = {
+      verify: function(authnr, otp, cb) {
+        return cb(null, true);
+      }
+    }
+    algorithms.use('example', algo);
+    
+    var strategy = new Strategy(algorithms, function(uid, done) {
+      var user = {
+        id: '501',
+        displayName: 'John Doe'
+      };
+      var authnr = {
+        id: 'dev_324598',
+        name: "Example Authenticator",
+        algorithm: 'foo'
+      };
+      return done(null, user, authnr);
+    });
+    
+    var err;
+    
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.body = { uid: '501', otp: '432028' };
+        })
+        .authenticate();
+    });
+    
+    it('should error', function() {
+      expect(err).to.be.an.instanceof(Error);
+      expect(err.message).to.equal('OTP algorithm "foo" is not supported');
+    });
+  }); // failing due to unsupported algorithm
+  
 });
